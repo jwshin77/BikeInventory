8fedc19241ad468de3347bcc18ee42a601d095b7
SKCC <SKCC>
2020-07-16 10:28:32 +0900

    commit

--- src/main/java/bike/BikeInventoryRepository.java
+++ src/main/java/bike/BikeInventoryRepository.java
@@ -2,7 +2,9 @@ package bike;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface BikeInventoryRepository extends PagingAndSortingRepository<BikeInventory, Long>{
 
-    //BikeInventory findByBikeInventoryId(Long id);
+    //List<BikeInventory> findByBikeInventoryId(Long id);
 }
